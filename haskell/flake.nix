e1446f5b285760c46b8b7ed59b0a516ae67a3f0a
Chris Chambers <magma.chambers@gmail.com>
2026-03-11 21:32:19 +0000

    Add bootstrap for Haskell projects

---
+++ haskell/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Haskell development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              cabal-install
+              ghc
+              haskell-language-server
+            ];
+          };
+        }
+      );
+    };
+}
